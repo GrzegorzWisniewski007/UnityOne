33eb661eb05296715c73635b83f6a5d0ad257d6e
GrzegorzDanielWisniewski <wisnia007@gmail.com>
2018-02-18 21:51:48 +0100

    Kilka rzeczy na androida

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private const float Y_ANGLE_MIN = 0.0f;
-    private const float Y_ANGLE_MAX = 50.0f;
+    private const float Y_ANGLE_MIN = 2.0f;
+    private const float Y_ANGLE_MAX = 80.0f;
+
+    private const float X_ANGLE_MIN = 2.0f;
+    private const float X_ANGLE_MAX = 80.0f;
 
 
     public Transform lookAt;
@@ -17,7 +20,10 @@ public class CameraController : MonoBehaviour
     private float currentX = 0.0f;
     private float currentY = -60.0f;
     private float sensivityX = 4.0f;
-    private float sensivityY = 1.0f;
+    private float sensivityY = 4.0f;
+
+    Vector3 gestureStartPosition;
+    Vector3 gestureDelta;
 
     private void Start()
     {
@@ -27,18 +33,54 @@ public class CameraController : MonoBehaviour
 
     private void Update()
     {
-        currentX += Input.GetAxis("Mouse X");
-        currentY += Input.GetAxis("Mouse Y");
+        //Debug.Log(Application.platform);
+        //if (Application.platform == RuntimePlatform.Android)
+        {
+            TouchScreen();
+        }
+        //else
+        {
+            //Mouse();
+        }          
 
-        //currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+        currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+
     }
 
     private void LateUpdate()
     {
-       // Debug.Log("Mouse X:" + currentX + " Mouse Y:" + currentY); 
         Vector3 dir = new Vector3(0, 0 + distance);
-        Quaternion rotation = Quaternion.Euler(currentY*sensivityY, currentX*sensivityX, 0 );
+        Quaternion rotation = Quaternion.Euler(currentY, currentX*sensivityX, 0 );
         camTransform.position = lookAt.position + rotation * dir;
         camTransform.LookAt(lookAt.position);
+    }
+
+    private void TouchScreen()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            gestureStartPosition = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            gestureDelta = Input.mousePosition - gestureStartPosition;
+            gestureDelta /= Screen.dpi;
+        }
+        else
+        {
+            gestureDelta = Vector3.zero;
+        }
+
+        currentX += gestureDelta.x;
+        currentY += gestureDelta.y;
+
+        //Debug.Log("GestureDelta: "+gestureDelta+" X: "+currentX+"Y: "+currentY);
+    }
+
+    private void Mouse()
+    {
+        currentX += Input.GetAxis("Mouse X");
+        currentY += Input.GetAxis("Mouse Y");
     }
 }
--- Assets/Scripts/MyCharacterController.cs
+++ Assets/Scripts/MyCharacterController.cs
@@ -6,7 +6,20 @@ public class MyCharacterController : MonoBehaviour
 {
     public CharacterController characterControler;
 
-    //Predkosc biegania.
+    public Camera Camera;
+
+    private float MoveButtons;
+
+    //przyciski sterowania na telefonie
+    //public GameObject ForwardButton;
+    //public GameObject BackwardButton;
+
+    private bool ForwardButton = false;
+    private bool BackwardButton = false;
+    private bool JumpButton = false;
+    private bool InAir = false;
+
+    //Predkosc.
     public float Velocity = 0f;
     //Prędkość poruszania się gracza.
     public float WalkSpeed = 5.0f;
@@ -17,57 +30,98 @@ public class MyCharacterController : MonoBehaviour
     //Aktualna wysokosc skoku.
     public float ActualJumpHeight = 0f;
 
+    private Animator animator;
+
     /** 
 	 * Pobranie prędkości poruszania się przód/tył.
 	 * jeżeli wartość dodatnia to poruszamy się do przodu,
 	 * jeżeli wartość ujemna to poruszamy się do tyłu.
 	 */
-    private float ForwardBackMovement;
-    /** 
-	 * Pobranie prędkości poruszania się lewo/prawo.
-	 * jeżeli wartość dodatnia to poruszamy się w prawo,
-	 * jeżeli wartość ujemna to poruszamy się w lewo.
-	 */
-    private float LeftRightMovement;
+    
+    private float JumpMove = 0.0f;
+
 
     /** Zmienna dostarcza informację o tym czy gracz bienie.*/
     public bool Run;
 
-    //Czulość myszki (Sensitivity)
+    //Czulość myszki 
     public float MouseSensitivy = 4.0f;
-    public float MouseUpDownMovement = 1.0f;
-    //Zakres patrzenia w górę i dół.
-    public float MouseUpDownRange = 90.0f;
+
+    Vector3 gestureStartPosition;
+    Vector3 gestureDelta;
+
+
 
     // Use this for initialization
     void Start()
     {
         characterControler = GetComponent<CharacterController>();
+        //ForwardButton = GetComponent<GameObject>();
+        //BackwardButton = GetComponent<GameObject>();
+
+        animator = (Animator)GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        klawiatura();
-        myszka();
+        if (ForwardButton)
+        {
+            MoveForward();
+        }
+        if (BackwardButton)
+        {
+            MoveBackward();
+        }
+        if (JumpButton)
+        {
+            Jump(0);            
+            Move(0);
+            if (characterControler.isGrounded && InAir)
+            {
+                JumpButton = false;
+            }
+        }
+
+        Mouse();
+
+        if(Application.platform != RuntimePlatform.Android && Application.platform != RuntimePlatform.WindowsEditor)
+        {
+            Keyboard();
+        }
+        
     }
 
-    private void klawiatura()
+    public void onPointerDownForwardButton()
     {
-        ForwardBackMovement = Input.GetAxis("Vertical") * Velocity;
-        LeftRightMovement = Input.GetAxis("Horizontal") * Velocity;
+        ForwardButton = true;
+    }
+    public void onPointerUpForwardButton()
+    {
+        Animation(0);
+        ForwardButton = false;
+    }
 
-        //Skakanie
-        // Jeżeli znajdujemy się na ziemi i została naciśnięta spacja (skok)
-        if (characterControler.isGrounded && Input.GetButton("Jump"))
-        {
-            ActualJumpHeight = JumpHeight;
-        }
-        else if (!characterControler.isGrounded)
-        {//Jezeli jestesmy w powietrzu(skok)
-         //Fizyka odpowiadająca za grawitacje (os Y).
-            ActualJumpHeight += Physics.gravity.y * Time.deltaTime;
-        }
+    public void onPointerDownBackwardButton()
+    {
+        BackwardButton = true;
+    }
+    public void onPointerUpBackwardButton()
+    {
+        Animation(0);
+        BackwardButton = false;
+    }
+
+    public void onClickJumpButton()
+    {
+        JumpButton = true;        
+    }
+
+    private void Keyboard()
+    {
+        float ForwardBackMovement = Input.GetAxis("Vertical") * Velocity;
+
+        ActualJumpHeight = Jump(ForwardBackMovement);
 
         //Bieganie
         if (Input.GetKeyDown("left shift"))
@@ -79,6 +133,78 @@ public class MyCharacterController : MonoBehaviour
             Run = false;
         }
 
+        Runing();
+        //Debug.Log(ForwardBackMovement);
+        Animation(ForwardBackMovement);        
+
+        Move(ForwardBackMovement);
+    }
+
+    private void Mouse()
+    {
+        //pobieranie wartości obrotu kamery
+        float CameraAngle = Camera.transform.rotation.eulerAngles.y;
+
+        float myszLewoPrawo = Input.GetAxis("Mouse X") * MouseSensitivy;
+        transform.rotation = Quaternion.Euler(0f, CameraAngle, 0f);
+    }
+
+    private void Move(float motion)
+    {
+        Vector3 move = new Vector3(0f, 0f, 0f);
+        if (characterControler.isGrounded)
+        {
+            move = new Vector3(0f, ActualJumpHeight, motion);
+        }
+        else
+        {
+            move = new Vector3(0f, ActualJumpHeight, JumpMove);
+        }
+
+        //Aktualny obrót gracza razy kierunek w którym sie poruszamy (poprawka na obrót myszką abyśmy szli w kierunku w którym patrzymy).
+        move = transform.rotation * move;
+
+        characterControler.Move(move * Time.deltaTime);
+    }
+
+    private void MoveForward()
+    {
+        Jump(Velocity);
+        Runing(); 
+        //Debug.Log(Velocity);
+        Animation(Velocity);
+        Move(Velocity);        
+    }
+
+    private void MoveBackward()
+    {
+        Jump(-Velocity);        
+        Move(-Velocity);
+        Animation(Velocity);
+    }
+
+    private float Jump(float move)
+    {
+        //Skakanie
+        if ((characterControler.isGrounded && Input.GetButton("Jump")) || (characterControler.isGrounded && JumpButton))
+        {
+            animator.SetTrigger("jump");
+            ActualJumpHeight = JumpHeight;
+            JumpMove = move;            
+        }
+        else if (!characterControler.isGrounded)
+        {
+            //Jezeli jestesmy w powietrzu(skok)
+            //Fizyka odpowiadająca za grawitacje (os Y).
+            InAir = true;
+            ActualJumpHeight += Physics.gravity.y * Time.deltaTime * 2;            
+        }
+
+        return ActualJumpHeight;
+    }
+
+    private void Runing()
+    {
         if (Run)
         {
             Velocity = RunningSpeed;
@@ -87,40 +213,11 @@ public class MyCharacterController : MonoBehaviour
         {
             Velocity = WalkSpeed;
         }
-
-        //Tworzymy wektor odpowiedzialny za ruch.
-        //rochLewoPrawo - odpowiada za ruch lewo/prawo,
-        //aktualnaWysokoscSkoku - odpowiada za ruch góra/dół,
-        //rochPrzodTyl - odpowiada za ruch przód/tył.
-        Vector3 ruch = new Vector3(LeftRightMovement, ActualJumpHeight, ForwardBackMovement);
-        
-        //Aktualny obrót gracza razy kierunek w którym sie poruszamy (poprawka na obrót myszką abyśmy szli w kierunku w którym patrzymy).
-        ruch = transform.rotation * ruch;
-
-        characterControler.Move(ruch * Time.deltaTime);
-
-        //Debug.Log(Input.GetAxis("Horizontal"));
-        transform.Rotate(0f, Input.GetAxis("Horizontal")* 90f, 0f);
-
     }
 
-    private void myszka()
-    {
-        //Pobranie wartości ruchu myszki lewo/prawo.
-        // jeżeli wartość dodatnia to poruszamy w prawo,
-        // jeżeli wartość ujemna to poruszamy w lewo.
-        float myszLewoPrawo = Input.GetAxis("Mouse X") * MouseSensitivy;
-        transform.Rotate(0, myszLewoPrawo, 0);
-
-        //Pobranie wartości ruchu myszki góra/dół.
-        // jeżeli wartość dodatnia to poruszamy w górę,
-        // jeżeli wartość ujemna to poruszamy w dół.
-        MouseUpDownMovement -= Input.GetAxis("Mouse Y") * MouseSensitivy;
-
-        //Funkcja nie pozwala aby wartość przekroczyła dane zakresy.
-        MouseUpDownMovement = Mathf.Clamp(MouseUpDownMovement, -MouseUpDownRange, MouseUpDownRange);
-        //Ponieważ CharacterController nie obraca się góra/dół obracamy tylko kamerę.
-        //Camera.main.transform.localRotation = Quaternion.Euler(MouseUpDownMovement, 0, 0);
+    private void Animation(float param)
+    {        
+        //sterownie animacjami chodzenia/biegania
+        animator.SetFloat("velocity", Mathf.Abs(param));
     }
-
 }
